9e27cd1b0716903f012f47c82da7e7ff31cb3753
tkiyo86 <32362106+tkiyo86@users.noreply.github.com>
2021-07-06 11:15:23 +0900

    43_event_style ボタンの配置やスタイルの記述方法

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 
 // コンポーネント化する、拡張子はJSXがよい
+
+/** 43
+1, button : {}で囲むとJSとして認識する
+2, sytle cssを書く
+　　style={{}}　　　一つ目の｛｝はＪＳを書く宣言
+　　　2つ目の｛｝はオブジェクトの｛｝
+ */
 const App = () => {
+  const onClickButton = () => alert(); 
+  const contentStyle = {
+    color: 'blue',
+    fontSize: '18px',
+  };
   return (
     <>
-      <h1>こんにちわ</h1>
-      <p>おげんきですか</p>
+      <h1 style={{color: 'red'}}>こんにちわ</h1>
+      <p style={contentStyle}>おげんきですか</p>
+      <button onClick={onClickButton}>押す</button>
     </>
   );
 };
