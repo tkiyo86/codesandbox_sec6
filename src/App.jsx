043b6c904a413672086ad04da364d583500c09d8
tkiyo86 <32362106+tkiyo86@users.noreply.github.com>
2021-07-06 15:24:57 +0900

    46_re-rendering 再レンダリング、useEffectを利用する、ある変数に着目してその値が変わった時だけレンダリングされる 論理演算子の使い方も大事

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,76 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ColorfulMessage from './components/ColorfulMessage';
 import ColorfulMessage2 from './components/ColorfulMessage2';
 
 // コンポーネント化する、拡張子はJSXがよい
 
-/** 45
-  usestate; コンポーネントの状態
-  引数に１，変数名、２，関数名（set***）
-  usestate(初期値)
-  ボタンを押すことでnumをsetNumで動的に更新したい
+/** 46
+ 再レンダリング
+ 再レンダリングの指示をしてないのにreactではStateが更新
+ ＝画面がこうしんされている
+ これはreact自身がコンポーネント内部で変更があれば再レンダリングしているから
+
+ 再レンダリングする条件は
+ １，Stateを変更したとき
+ ２，Propsの中身が変わったとき
+ ３，親コンポが再レンダされると子コンポも再レンダリングされる
  */
 const App = () => {
+  console.log("コンポ―ネントのはじまり");
+  const [num, setNum] = useState(0);  
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
   const onClickCountup = () => {
     setNum(num+1);
   };
-  const [num, setNum] = useState(0);  
+
+  // !でnot True/Falseを切り替えている
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+/**
+カウントアップして3の倍数の時だけ
+表示するようにしたい
+
+単純なIF文で対処するとレンダリング回数が多すぎてエラー
+Error
+Too many re-renders. 
+React limits the number of renders to prevent an infinite loop.
+ 
+  if (num%3 === 0) {
+    setFaceShowFlag(true);
+  } else {
+    setFaceShowFlag(false);
+  };
+  */
+
+// useEffect 第二引数で空の配列　＝　はじめの一回だけread
+// [num] とするとnumの値が変わった時だけ実行される
+// ボタンON/OFFを変更しても影響はない、numだけを見て実行される
+  useEffect(() => {
+    if (num > 0) {
+      if (num%3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    //console.log("useEffect!",{num})
+  }, [num]);
+
+
+  // 画面のレンダリング
   return (
     <>
       <h1 style={{color: 'red'}}>こんにちわ</h1>
       <ColorfulMessage2 color="blue">お元気ですか？</ColorfulMessage2>
       <ColorfulMessage color="green" message="元気です"/>
       <button onClick={onClickCountup}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>46再レンダリング</p>}
     </>
   );
 };
