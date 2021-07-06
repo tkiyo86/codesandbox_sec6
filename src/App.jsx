33e3c8b9c9bacc0b4c6c32bfb48f66276b844a15
tkiyo86 <32362106+tkiyo86@users.noreply.github.com>
2021-07-06 13:53:02 +0900

    45_State useStateでボタン押すと表示する数を動的に変更する

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ColorfulMessage from './components/ColorfulMessage';
 import ColorfulMessage2 from './components/ColorfulMessage2';
 
 // コンポーネント化する、拡張子はJSXがよい
 
-/** 44
-  コンポーネント＋Propsで再利用しやすい形に
-  ColorfulMessage で色と文章を引数にする
+/** 45
+  usestate; コンポーネントの状態
+  引数に１，変数名、２，関数名（set***）
+  usestate(初期値)
+  ボタンを押すことでnumをsetNumで動的に更新したい
  */
 const App = () => {
-  const onClickButton = () => alert();  
+  const onClickCountup = () => {
+    setNum(num+1);
+  };
+  const [num, setNum] = useState(0);  
   return (
     <>
       <h1 style={{color: 'red'}}>こんにちわ</h1>
       <ColorfulMessage2 color="blue">お元気ですか？</ColorfulMessage2>
       <ColorfulMessage color="green" message="元気です"/>
-      <button onClick={onClickButton}>押す</button>
+      <button onClick={onClickCountup}>カウントアップ</button>
+      <p>{num}</p>
     </>
   );
 };
